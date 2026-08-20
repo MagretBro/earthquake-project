aad08b8488be4ef5adc3f42a5166f86db191c9e7
Magret Bro <magretbro@gmail.com>
2026-08-20 21:39:47 +0300

    final

--- dags/fct_avg_day_earthquake.py
+++ dags/fct_avg_day_earthquake.py
@@ -7,56 +7,64 @@ from airflow.sensors.external_task import ExternalTaskSensor
 OWNER = 'magret_bro'
 DAG_ID = 'fct_avg_day_earthquake'
 
-# using table DAG
-LAYER = 'raw'
-SOURCE = 'earthquake'
-SCHEMA = 'dm'
-TARGET_TABLE = 'fct_avg_day_earthquake'
+# Используемые таблицы в DAG
+LAYER = "raw"
+SOURCE = "earthquake"
+SCHEMA = "dm"
+TARGET_TABLE = "fct_avg_day_earthquake"
 
 # DWH
-PG_CONNECT = 'postgres_dwh'
-LONG_DESCRIPTION = """This DAG calculates the average earthquake magnitude for each day."""
-SHORT_DESCRIPTION = """This DAG calculates the average earthquake magnitude for each day."""
+PG_CONNECT = "postgres_dwh"
+
+LONG_DESCRIPTION = """
+# LONG DESCRIPTION
+"""
+
+SHORT_DESCRIPTION = "SHORT DESCRIPTION"
 
 args = {
-    'owner': OWNER,
-    'start_date': pendulum.datetime(2026, 8, 18, tz="UTC"),
-    'catchup': True,
-    'retries': 3,
-    'retry_delay': pendulum.duration(hours=1),
+    "owner": OWNER,
+    "start_date": pendulum.datetime(2026, 8, 15, tz="Europe/Moscow"),
+    "catchup": True,
+    "retries": 3,
+    "retry_delay": pendulum.duration(hours=1),
 }
 
-with DAG (
+
+
+with DAG(
     dag_id=DAG_ID,
-    schedule_interval='@daily',
+    schedule_interval="0 5 * * *",
     default_args=args,
+    tags=["dm", "pg"],
     description=SHORT_DESCRIPTION,
-    tags=['dm', 'pg'],
     concurrency=1,
     max_active_tasks=1,
     max_active_runs=1,
 ) as dag:
     dag.doc_md = LONG_DESCRIPTION
 
-    start = EmptyOperator(task_id='start')
+    start = EmptyOperator(
+        task_id="start",
+    )
 
     sensor_on_raw_layer = ExternalTaskSensor(
-        task_id='sensor_on_raw_layer',
-        external_dag_id='raw_from_s3_to_pg',
-        allowed_states=['success'],
-        mode='reschedule',
-        poke_interval=60,
-        timeout=60,
+        task_id="sensor_on_raw_layer",
+        external_dag_id="raw_from_s3_to_pg",
+        allowed_states=["success"],
+        mode="reschedule",
+        timeout=360000,  # длительность работы сенсора
+        poke_interval=60,  # частота проверки
     )
 
     drop_stg_table_before = SQLExecuteQueryOperator(
-            task_id="drop_stg_table_before",
-            conn_id=PG_CONNECT,
-            autocommit=True,
-            sql=f"""
-            DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
-            """,
-        )
+        task_id="drop_stg_table_before",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
 
     create_stg_table = SQLExecuteQueryOperator(
         task_id="create_stg_table",
@@ -75,16 +83,16 @@ with DAG (
         """,
     )
 
-
-
     drop_from_target_table = SQLExecuteQueryOperator(
-        task_id='drop_from_target_table',
+        task_id="drop_from_target_table",
         conn_id=PG_CONNECT,
         autocommit=True,
-        sql=f"""DELETE FROM {SCHEMA}.{TARGET_TABLE} 
-        WHERE date in (
+        sql=f"""
+        DELETE FROM {SCHEMA}.{TARGET_TABLE}
+        WHERE date IN
+        (
             SELECT date FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
-        ) 
+        )
         """,
     )
 
@@ -107,39 +115,17 @@ with DAG (
         """,
     )
 
-
     end = EmptyOperator(
         task_id="end",
     )
 
     (
-        start >>
-        sensor_on_raw_layer >>
-        drop_stg_table_before >>
-        create_stg_table >>
-        drop_from_target_table >>
-        insert_into_target_table >>
-        drop_stg_table_after >>
-        end
+            start >>
+            sensor_on_raw_layer >>
+            drop_stg_table_before >>
+            create_stg_table >>
+            drop_from_target_table >>
+            insert_into_target_table >>
+            drop_stg_table_after >>
+            end
     )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
